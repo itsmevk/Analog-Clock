04fbe9511f09ce2fabdd63cd5fb20be8513880f7
Vishwajeet Kumar <56990458+itsmevk@users.noreply.github.com>
2022-05-25 20:50:03 +0530

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,23 @@
+setInterval(setClock,1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minHand = document.querySelector('[data-min-hand]')
+const secHand = document.querySelector('[data-sec-hand]')
+
+function setClock(){
+    const currentDate = new Date()
+    const secondRatio =currentDate.getSeconds()/60
+    const minRatio = (secondRatio+ currentDate.getMinutes())/60
+    const hourRatio = (minRatio + currentDate.getHours())/12
+
+    setRotation(secHand,secondRatio)
+    setRotation(minHand,minRatio)
+    setRotation(hourHand,hourRatio)
+
+
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio*360)
+}
+setClock()
